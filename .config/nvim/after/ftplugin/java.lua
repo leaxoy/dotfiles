3cb942c0fb1827663a26af42a28768faa572d48b
leaoxy <lixiaohui0812@gmail.com>
2022-09-29 08:55:40 +0800

    update java config

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -1,12 +1,15 @@
-local cwd = vim.fn.getcwd()
+local status, jdtls = pcall(require, "jdtls")
+if not status then return end
 
-require("jdtls").start_or_attach {
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+
+jdtls.start_or_attach {
   cmd = {
     "jdtls",
     "-configuration",
     os.getenv "HOME" .. "/.cache/jdtls/config",
     "-data",
-    os.getenv "HOME" .. "/.cache/jdtls/workspace/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
+    os.getenv "HOME" .. "/.cache/jdtls/workspace/" .. project_name,
   },
   settings = {
     java = {
@@ -17,3 +20,10 @@ require("jdtls").start_or_attach {
     bundles = {},
   },
 }
+
+jdtls.setup.add_commands()
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = jdtls.organize_imports,
+  desc = "Organize Imports",
+})
