fdd2aa0e31a8dc41016c94b8ce8b4f40b0ee0eb7
lixiaohui <lixiaohui.1024@bytedance.com>
2022-10-27 11:54:03 +0800

    nvim: exchange lsp client & server

--- .config/nvim/after/plugin/lsp/client.lua
+++ .config/nvim/after/plugin/lsp/client.lua
@@ -1,93 +1,184 @@
-local lsp_status, lsp = pcall(require, "lspconfig")
-if not lsp_status then return end
+local function resolve_lsp_command(cmds, lang)
+  return function()
+    vim.ui.select(cmds, {
+      prompt = "Execute Commands:",
+    }, function(choice)
+      if not choice then return end
+      vim.api.nvim_out_write("Execute command: " .. choice)
+      if vim.bo.filetype == "go" then
+        local arg = { URI = vim.uri_from_bufnr(vim.api.nvim_get_current_buf()) }
+        vim.lsp.buf.execute_command { command = choice, arguments = { arg } }
+      end
+    end)
+  end
+end
 
-local mason_status, mason_adapter = pcall(require, "mason-lspconfig")
-if not mason_status then return end
+local function resolve_text_document_capabilities(client, buffer)
+  local map = function(mode, lhs, rhs, opts)
+    if type(mode) == "string" and string.len(mode) > 1 then mode = vim.split(mode, "") end
+    opts = vim.tbl_extend("force", { noremap = true, silent = true, buffer = buffer }, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
 
--- Add lsp info border
-local win_opts = require("lspconfig.ui.windows").default_options
-win_opts.border = "double"
+  local caps = client.server_capabilities
 
-mason_adapter.setup {
-  automatic_installation = true,
-  ensure_installed = vim.g.lsp_servers or {},
-}
+  if caps.declarationProvider then
+    map("n", "gD", vim.lsp.buf.declaration, { desc = "Declaration" })
+  end
+  if caps.definitionProvider then
+    map("n", "gd", vim.lsp.buf.definition, { desc = "Definition" })
+  end
+  if caps.typeDefinitionProvider then
+    map("n", "gt", vim.lsp.buf.type_definition, { desc = "Type Definition" })
+  end
+  -- if caps.implementationProvider then
+  --   map("n", "gi", vim.lsp.buf.implementation, { desc = "Implementation" })
+  -- end
+  -- if caps.referencesProvider then
+  --   map("n", "gr", vim.lsp.buf.references, { desc = "References" })
+  -- end
+  if caps.callHierarchyProvider then
+    map("n", "ghi", vim.lsp.buf.incoming_calls, { desc = "Incoming Calls" })
+    map("n", "gho", vim.lsp.buf.outgoing_calls, { desc = "Outgoing Calls" })
+  end
+  -- if caps.typeHierarchyProvider then
+  -- end
+  if caps.documentHighlightProvider then
+    local group = "lsp_document_highlight"
+    vim.api.nvim_create_augroup(group, { clear = false })
+    vim.api.nvim_clear_autocmds { buffer = buffer, group = group }
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      group = group,
+      buffer = buffer,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      group = group,
+      buffer = buffer,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
+  -- if caps.documentLinkProvider then
+  -- end
+  if caps.hoverProvider then
+    local hover = function()
+      if vim.tbl_contains({}, vim.bo.filetype) then
+        vim.cmd.help { args = { vim.fn.expand "<cword>" } }
+      elseif vim.bo.filetype == "man" then
+        vim.cmd.Man { args = { vim.fn.expand "<cword>" } }
+      elseif vim.fn.expand "%:t" == "Cargo.toml" then
+        require("crates").show_popup()
+      else
+        vim.lsp.buf.hover()
+      end
+    end
+    map("n", "K", hover, { desc = "Hover" })
+  end
+  if caps.codeLensProvider then
+    vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged" }, {
+      buffer = buffer,
+      callback = function() vim.lsp.codelens.refresh() end,
+    })
+    vim.lsp.codelens.refresh()
+    map("n", "gad", vim.lsp.codelens.display, { desc = "Display CodeLens" })
+    map("n", "gar", vim.lsp.codelens.run, { desc = "Run CodeLens" })
+    map("n", "gaf", vim.lsp.codelens.refresh, { desc = "Refresh CodeLens" })
+  end
+  -- if caps.foldingRangeProvider then
+  -- end
+  -- if caps.selectionRangeProvider then
+  -- end
+  if caps.documentSymbolProvider then
+    local status, outline = pcall(require, "lspsaga.outline")
+    if status then
+      map("n", "go", function() outline:render_outline() end, { desc = "Document Symbol" })
+    else
+      map("n", "go", vim.lsp.buf.document_symbol, { desc = "Document Symbol" })
+    end
+  end
+  if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+    local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
+    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave", "TextChanged" }, {
+      group = augroup,
+      buffer = buffer,
+      callback = vim.lsp.buf.semantic_tokens_full,
+    })
+    vim.lsp.buf.semantic_tokens_full()
+  end
+  -- if caps.inlineValueProvider then
+  -- end
+  if caps.inlayHintProvider then
+    local status, hint = pcall(require, "lsp-inlayhints")
+    if status then hint.on_attach(client, buffer, false) end
+  end
+  -- if caps.monikerProvider then
+  -- end
+  -- if caps.completionProvider then
+  -- end
+  -- if caps.diagnosticProvider then
+  -- end
+  if caps.signatureHelpProvider then
+    map("n", "gs", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+  end
+  if caps.codeActionProvider then
+    local code_action = function() vim.lsp.buf.code_action { apply = true } end
+    map("nv", "<leader>a", code_action, { desc = "Code Action" })
+  end
+  if caps.colorProvider then
+    local document_color_status, document_color = pcall(require, "document-color")
+    if document_color_status then document_color.buf_attach(buffer) end
+  end
+  if caps.documentFormattingProvider then
+    local format_group = "document_formatting"
+    vim.api.nvim_create_augroup(format_group, { clear = false })
+    vim.api.nvim_clear_autocmds { buffer = buffer, group = format_group }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = format_group,
+      buffer = buffer,
+      command = "lua vim.lsp.buf.format()",
+    })
+  end
+  -- if caps.documentRangeFormattingProvider then
+  -- end
+  -- if caps.documentOnTypeFormattingProvider then
+  -- end
+  if caps.renameProvider then map("n", "gr", vim.lsp.buf.rename, { desc = "Rename" }) end
+  -- if caps.linkedEditingRangeProvider then
+  -- end
+end
 
-mason_adapter.setup_handlers {
-  function(server_name)
-    local excludes = { "jdtls", "rust_analyzer" }
-    if vim.tbl_contains(excludes, server_name) then return end
-    lsp[server_name].setup {}
+local function resolve_workspace_capabilities(client, buffer)
+  local map = function(mode, lhs, rhs, opts)
+    opts = vim.tbl_extend("force", { noremap = true, silent = true, buffer = buffer }, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+
+  local caps = client.server_capabilities
+
+  --#region workspace start
+  if caps.workspaceSymbolProvider then
+    map("n", "gO", vim.lsp.buf.workspace_symbol, { desc = "Workspace Symbol" })
+  end
+  if caps.executeCommandProvider then
+    -- local cmds = caps.executeCommandProvider.commands
+    -- local langs = client.config.filetypes
+    -- map("n", "ge", resolve_lsp_command(cmds, langs), { desc = "Execute Command" })
+  end
+  if caps.workspace and caps.workspace.workspaceFolders then
+    map("n", "gwa", vim.lsp.buf.add_workspace_folder, { desc = "Add Workspace" })
+    map("n", "gwr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove Workspace" })
+    local print_workspaces = function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end
+    map("n", "gwl", print_workspaces, { desc = "List Workspace" })
+  end
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local buf = args.buf
+
+    resolve_text_document_capabilities(client, buf)
+    resolve_workspace_capabilities(client, buf)
   end,
-  clangd = function()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.offsetEncoding = { "utf-18" }
-    lsp.clangd.setup { capabilities = capabilities }
-  end,
-  gopls = function()
-    lsp.gopls.setup {
-      init_options = { allExperiments = true },
-      settings = {
-        gopls = {
-          -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
-          allExperiments = true,
-          deepCompletion = true,
-        },
-      },
-    }
-  end,
-  jsonls = function()
-    lsp.jsonls.setup { settings = { json = { schemas = require("schemastore").json.schemas() } } }
-  end,
-  omnisharp = function()
-    lsp.omnisharp.setup {
-      cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/OmniSharp.dll" },
-    }
-  end,
-  rust_analyzer = function()
-    require("rust-tools").setup {
-      tools = { inlay_hints = { auto = false } },
-      server = { standalone = true },
-      dap = {
-        adapter = {
-          type = "executable",
-          command = "codelldb",
-          name = "codelldb",
-        },
-      },
-    }
-  end,
-  sumneko_lua = function()
-    require("neodev").setup {}
-    local root_dir = require("lspconfig").util.root_pattern("init.lua", "lua")
-    lsp.sumneko_lua.setup { root_dir = root_dir }
-  end,
-  tsserver = function()
-    lsp.tsserver.setup {
-      settings = {
-        typescript = {
-          inlayHints = {
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
-          },
-        },
-        javascript = {
-          inlayHints = {
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
-          },
-        },
-      },
-    }
-  end,
-}
+  desc = "setup lsp functions",
+})
