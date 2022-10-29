eae8d6795fea76751c11cbe9fb45edd9abf5613c
lixiaohui <lixiaohui.1024@bytedance.com>
2022-10-29 10:55:41 +0800

    nvim: update noice

--- .config/nvim/after/plugin/ui/noice.lua
+++ .config/nvim/after/plugin/ui/noice.lua
@@ -1,24 +1,17 @@
 local status, noice = pcall(require, "noice")
 if not status then return end
 
-local function lsp_opts(title)
-  return {
-    border = { text = { top = title or "" } },
-  }
-end
-
 noice.setup {
-  cmdline = {
-    format = {
-      input = {
-        opts = { relative = "cursor", position = { col = 1, row = 1 }, size = { min_width = 20 } },
-      },
-    },
-  },
+  cmdline = { opts = { size = { min_width = 20 } } },
   lsp = {
-    hover = { enabled = true, opts = lsp_opts "Hover" },
-    signature = { enabled = true, opts = lsp_opts "Signature" },
-    documentation = { view = "hover" },
+    override = {
+      -- override the default lsp markdown formatter with Noice
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      -- override the lsp markdown formatter with Noice
+      ["vim.lsp.util.stylize_markdown"] = true,
+      -- override cmp documentation with Noice (needs the other options to work)
+      ["cmp.entry.get_documentation"] = true,
+    },
   },
   messages = { enabled = true, view = false, view_warn = false, view_error = false },
   popupmenu = { backend = "nui" },
@@ -26,12 +19,7 @@ noice.setup {
     bottom_search = false,
     command_palette = false,
     long_message_to_split = true,
-  },
-  views = {
-    hover = {
-      border = { style = "double", padding = { 0, 0, 0, 0 } },
-      position = { row = 1, col = 1 },
-    },
+    -- lsp_doc_border = true,
   },
 }
 
