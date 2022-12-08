e508ad6740c0750bf17b9f0f67aed4157d47dc42
lixiaohui <lixiaohui.1024@bytedance.com>
2022-12-08 23:58:10 +0800

    nvim: update null-ls config

--- .config/nvim/lua/setting/null_ls.lua
+++ .config/nvim/lua/setting/null_ls.lua
@@ -1,5 +1,16 @@
 local SettingResitry = require "neoconf.plugins"
 
+---@class NullLsSetting
+---@field enabled boolean
+---@field extra_args table<string>
+---@field extra_filetypes table<string>
+---@field disabled_filetypes table<string>
+---@alias NullLsSettingGroup table<string, NullLsSetting>
+---@class NullLsSettingSection
+---@field code_action NullLsSettingGroup
+---@field diagnostic NullLsSettingGroup
+---@field formatting NullLsSettingGroup
+
 SettingResitry.register {
   on_schema = function(schema)
     schema:set("null_ls", {
@@ -19,72 +30,78 @@ SettingResitry.register {
       type = "object",
     })
 
+    local ft_options = require "null-ls.builtins._meta.filetype_map"
+    local all_filetypes = vim.tbl_keys(ft_options)
+
     local default_options = {
-      enabled = { type = "boolean", description = "whether enabled" },
+      enabled = { type = "boolean", description = "Whether enabled" },
       extra_args = {
         type = "array",
+        description = "additional args passed to cli tool",
         items = { type = "string" },
       },
       extra_filetypes = {
         type = "array",
-        items = { type = "string" },
+        description = "additional filetypes passed to cli tool",
+        items = { type = "string", enum = all_filetypes },
       },
       disabled_filetypes = {
         type = "array",
-        items = { type = "string" },
+        description = "disabled filetypes for cli tool",
+        items = { type = "string", enum = all_filetypes },
       },
     }
-    schema:set("null_ls.code_action.gitsigns", {
-      description = "gitsigns code action",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.code_action.refactoring", {
-      description = "refactoring",
-      type = "object",
-      properties = default_options,
-    })
 
-    schema:set("null_ls.diagnostic.fish", {
-      description = "fish shell script diagnostic",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.diagnostic.golangci_lint", {
-      description = "golangci_lint",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.diagnostic.ruff", {
-      description = "python ruff linter",
-      type = "object",
-      properties = default_options,
-    })
+    local templete = {
+      with_source = "%s: %s\nDescription: %s\nHomepage: %s\nDefault Filetypes: [%s]",
+      without_source = "%s: %s\nDefault Filetypes: [%s]",
+    }
 
-    schema:set("null_ls.formatter.black", {
-      description = "The uncompromising Python code formatter",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.formatter.fish_indent", {
-      description = "fish formatter options",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.formatter.jq", {
-      description = "Command-line JSON processor",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.formatter.isort", {
-      description = "A Python utility / library to sort imports",
-      type = "object",
-      properties = default_options,
-    })
-    schema:set("null_ls.formatter.stylua", {
-      description = "An opinionated Lua code formatter",
-      type = "object",
-      properties = default_options,
-    })
+    ---@param type_ string
+    ---@param name string
+    ---@param module string
+    ---@param fts table|nil
+    local function desc(type_, name, module, fts)
+      local status, source = pcall(require, "null-ls.builtins." .. module .. "." .. name)
+      if status then
+        return string.format(
+          templete.with_source,
+          type_,
+          name,
+          source.meta and source.meta.description or "",
+          source.meta and source.meta.url or "",
+          table.concat(fts or { "all" }, ", ")
+        )
+      else
+        return string.format(
+          templete.without_source,
+          type_,
+          name,
+          table.concat(fts or { "all" }, ", ")
+        )
+      end
+    end
+
+    for name, cfg in pairs(require "null-ls.builtins._meta.code_actions") do
+      schema:set("null_ls.code_action." .. name, {
+        description = desc("CodeAction", name, "code_actions", cfg.filetypes),
+        type = "object",
+        properties = default_options,
+      })
+    end
+    for name, cfg in pairs(require "null-ls.builtins._meta.diagnostics") do
+      schema:set("null_ls.diagnostic." .. name, {
+        description = desc("Diagnostic", name, "diagnostics", cfg.filetypes),
+        type = "object",
+        properties = default_options,
+      })
+    end
+    for name, cfg in pairs(require "null-ls.builtins._meta.formatting") do
+      schema:set("null_ls.formatter." .. name, {
+        description = desc("Formatter", name, "formatting", cfg.filetypes),
+        type = "object",
+        properties = default_options,
+      })
+    end
   end,
 }
