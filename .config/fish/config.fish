8f4c02a3eb75adb4abb66492e1992c8c60919e53
lixiaohui <lixiaohui.1024@bytedance.com>
2023-07-24 19:32:25 +0800

    update alacritty & fish & tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,21 @@
 if status is-interactive
     # PATH setting
-    set -g fish_user_paths /usr/local/opt/llvm/bin $fish_user_paths
-    set JAVA_HOME /usr/local/opt/openjdk
-    set -g fish_user_paths $JAVA_HOME/bin $fish_user_paths
-    set -g fish_user_paths $HOME/.jbang/bin $fish_user_paths
-    set -g fish_user_paths $HOME/Library/Python/3.10/bin $fish_user_paths
     set -g fish_user_paths $HOME/go/bin $fish_user_paths
     set -g fish_user_paths $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/bin $fish_user_paths
     set -g fish_user_paths $HOME/.cargo/bin $fish_user_paths
-    # ghc
-    set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-    set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
     # pnpm
-    set -gx PNPM_HOME /Users/bytedance/Library/pnpm
+    set -gx PNPM_HOME $HOME/Library/pnpm
     set -gx PATH "$PNPM_HOME" $PATH
     # Bun
     set -Ux BUN_INSTALL "$HOME/.bun"
     set -px --path PATH "$HOME/.bun/bin"
 
-    command -qv goenv && source (goenv init - | psub)
-    command -qv oh-my-posh && oh-my-posh init fish | source
-    command -qv starship && starship init fish | source
     command -qv zoxide && zoxide init fish | source
+    command -qv direnv && direnv hook fish | source
+    command -qv starship && starship init fish | source
+    # command -qv oh-my-posh && oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/gruvbox.omp.json | source
+    [ -f "$HOME/.bytebm/config/fish.sh" ]; and source "$HOME/.bytebm/config/fish.sh"
+    source (brew --prefix asdf)/libexec/asdf.fish
 
     # ALIAS setting
     command -qv bat && alias cat bat # https://github.com/sharkdp/bat
@@ -32,9 +26,13 @@ if status is-interactive
     command -qv rg && alias grep rg # https://github.com/BurntSushi/ripgrep
     command -qv procs && alias ps procs # https://github.com/dalance/procs
     command -qv sd && alias sed sd # https://github.com/chmln/sd
+    command -qv tmux && alias ta "tmux at"
     if command -qv nvim
         set -gx EDITOR nvim
         alias vim nvim
+        if command -qv fzf
+            alias vf 'nvim $(fzf)'
+        end
     end
 
     # Local config
@@ -51,4 +49,12 @@ if status is-interactive
             set platform_local (dirname (status --current-filename))/config.windows.fish
     end
     test -f $platform_local && source $platform_local
+
+    # Base16 Shell
+    # set BASE16_SHELL_PATH "$HOME/.config/base16-shell"
+    # if test -s "$BASE16_SHELL_PATH"
+    #     source "$BASE16_SHELL_PATH/profile_helper.fish"
+    #     set -gx BASE16_THEME_DEFAULT gruvbox-dark-dark
+    #     set -gx BASE16_THEME gruvbox-dark-dark
+    # end
 end
